e4e0ac7ef3be137d7b37dec56f783dabbdaf05fb
mbgame <mbgame@users.noreply.github.com>
2024-05-07 07:49:45 +0000

    deploy: fb78ed27216c48eda4d7c9ad8f8cbeebb25d1e9a

--- server/server-reference-manifest.js
+++ server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"B7y4cW3PbUK6V1ShfIrCbVl5TKT3vI87QgPNOqEjuOg=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"1H3MUzkbHUqvZL73SFhfk0Q2gJKN+5u1kX8Pf4UsgvI=\"}"
